51b6f9cb0df6bf2a0693a771fb6b63ea08a64e21
igor <ehotsureev@gmail.com>
2023-11-16 10:19:33 +0300

    change function and requirements.txt

--- parser_app/pars_phone_from_halooglasi.py
+++ parser_app/pars_phone_from_halooglasi.py
@@ -2,18 +2,22 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
 from parser_app.models.marketing import Marketing
+from parser_app.parsing_app import marketing_connector
 
 
-def get_phone_and_spacename_list(driver: Chrome, links_list: list) -> list:
+def parce_from_links_and_save_in_db(driver: Chrome, links_list: list):
     space_info_list = []
     for link in links_list:
-        driver.get(link)
-        driver.find_element(By.XPATH, '//span[@class="show-phone-span"]').click()
-        driver.implicitly_wait(1.0)
-        phone_element = driver.find_element(By.XPATH, '//a[contains(@href, "tel:")]')
-        name_element = driver.find_element(By.XPATH, '//span[@id="plh1" and @data-code="Title"]')
-        phone_dict = {"name": name_element.text, "phone": phone_element.text}
-        space_info = Marketing(**phone_dict)
-        space_info_list.append(space_info)
-
-    return space_info_list
+        try:
+            driver.get(link)
+            driver.find_element(By.XPATH, '//span[@class="show-phone-span"]').click()
+            driver.implicitly_wait(1.0)
+            phone_element = driver.find_element(By.XPATH, '//a[contains(@href, "tel:")]')
+            name_element = driver.find_element(
+                By.XPATH, '//span[@id="plh1" and @data-code="Title"]'
+            )
+            phone_dict = {"name": name_element.text, "phone": phone_element.text}
+            marketing_connector.save(Marketing(**phone_dict))
+        except Exception as e:
+            print(e)
+            continue
--- parser_app/parsing_app.py
+++ parser_app/parsing_app.py
@@ -1,9 +1,7 @@
 from selenium.webdriver import Chrome, ChromeOptions
 
 from parser_app.links_list import links_for_parser
-from parser_app.pars_phone_from_halooglasi import get_phone_and_spacename_list
-from parser_app.postgres.connectors.marketing_connector import MarketingConnector
-from parser_app.postgres.database import get_database
+from parser_app.pars_phone_from_halooglasi import parce_from_links_and_save_in_db
 
 options = ChromeOptions()
 options.add_argument("--no-sandbox")
@@ -16,10 +14,7 @@ options.add_argument("window-size=1920,1080")
 options.add_argument("--disableself.proxy-infobars")
 options.add_argument("--dns-prefetch-disable")
 
-marketing_connector = MarketingConnector(get_database())
 driver = Chrome(options=options)
 
 if __name__ == "__main__":
-    space_info = get_phone_and_spacename_list(driver=driver, links_list=links_for_parser)
-    for space in space_info:
-        marketing_connector.save(space)
+    space_info = parce_from_links_and_save_in_db(driver=driver, links_list=links_for_parser)
